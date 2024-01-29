a6a2d4dda253c6125b34ba226e1d00d4de53670e
Waliia <waliazhar45@gmail.com>
2024-01-29 17:29:46 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
