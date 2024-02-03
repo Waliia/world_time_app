41f4afd58269ebbe84ab2b331b7a36d1e3d739db
Waliia <waliazhar45@gmail.com>
2024-02-03 18:55:51 +0500

    error handling in api

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -14,21 +14,26 @@ class WorldTime {
     String baseURL = 'http://worldtimeapi.org/api/timezone/';
     // String endURL = 'Asia/Karachi';
     //
-    Response response;
-    response = await get(Uri.parse(baseURL + url));
+    try {
+      Response response;
+      response = await get(Uri.parse(baseURL + url));
 
-    Map data = jsonDecode(response.body);
-    // print(data);
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    String datetime = data["datetime"];
-    String offset = data["utc_offset"].substring(1, 3);
-    // print(datetime);
-    // print(offset);
+      String datetime = data["datetime"];
+      String offset = data["utc_offset"].substring(1, 3);
+      // print(datetime);
+      // print(offset);
 
-    DateTime date = DateTime.parse(datetime);
-    date = date.add(Duration(hours: int.parse(offset)));
-    // print(date);
+      DateTime date = DateTime.parse(datetime);
+      date = date.add(Duration(hours: int.parse(offset)));
+      // print(date);
 
-    time = date.toString();
+      time = date.toString();
+    } catch (e) {
+      print('caught error: $e');
+      time = 'could not get time zone';
+    }
   }
 }
